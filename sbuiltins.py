db2c52b8cb7cff682c0f451247b28cea6064e5ab
Vadim Kramer <vkokielov@gmail.com>
2017-05-31 13:11:51 -0400

    easier predicates/typechecks

--- sbuiltins.py
+++ sbuiltins.py
@@ -24,6 +24,16 @@ def bind_builtin(env,name,pyf,check_arg_count=None, check_arg_types=None, is_raw
     proc = sdata.PythonProc(env,name,pyf,check_arg_count, check_arg_types, is_raw)
     env.bind(name,proc)
 
+def make_checker(test):
+    def _check(value,index):
+        return test(value)
+    return _check
+
+def make_predicate(test):
+    def _predicate(env,tokens,arg):
+        return test(arg)
+    return _predicate
+
 def py_accumulate(op,inputs):
     if not inputs:
         return None
@@ -76,7 +86,7 @@ def pair_left(env,token,arg):
 def pair_right(env,token,arg):
     return arg[0].cdr
 
-def is_eq(env,token,left,right):
+def is_eq(left,right):
     return left == right   # The most straightorward way is to compare Python references
 
 def make_list(env,token,args):
@@ -112,6 +122,9 @@ def is_pair(value):
 def is_list(value):
     return value == sdata.the_empty_list or is_pair(value)
 
+def is_empty_list(value):
+    return value == sdata.the_empty_list
+
 def register_builtins(env):
     bind_builtin(env,'+',make_accumulator(operator.add),\
                   check_arg_count=(1,None))
@@ -125,8 +138,9 @@ def register_builtins(env):
     bind_builtin(env,'/',make_accumulator(operator.truediv),\
                   check_arg_count=(1,None))
 
-    check_pair = (lambda arg,idx: is_pair(arg))
-    check_list = (lambda arg,idx: is_list(arg))
+    check_pair = make_checker(is_pair)
+    check_list = make_checker(is_list)
+    
     check_int = (lambda arg,idx: isinstance(arg,sdata.SkimpyNumber) and type(arg.value) == int)
     check_procedure = (lambda arg,idx: isinstance(arg,sdata.SkimpyProc))
 
@@ -142,7 +156,7 @@ def register_builtins(env):
     bind_builtin(env,'cdr',pair_right,check_arg_count=1,
                  check_arg_types=[('pair',check_pair)], is_raw=True)
 
-    bind_builtin(env,'null?',(lambda env,tok,l: l == sdata.the_empty_list),check_arg_count=1)
+    bind_builtin(env,'null?',make_predicate(is_empty_list),check_arg_count=1)
     bind_builtin(env,'eq?',is_eq,check_arg_count=2)
     # The list operations
     bind_builtin(env,'map',map_list,check_arg_count=2,
